c1144ab0a3a02cf938cfb2ccf007f734e703645a
MuhammedAnwarsha <muhammedanwarshab@gmaill.com>
2024-07-03 15:58:17 +0530

    set corsConfigurationSource

--- src/main/java/com/trade/config/AppConfig.java
+++ src/main/java/com/trade/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.trade.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,7 +9,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class AppConfig {
@@ -28,7 +33,26 @@ public class AppConfig {
 
     private CorsConfigurationSource corsConfigurationSource() {
 
-        return null;
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                CorsConfiguration cfg = new CorsConfiguration();
+
+                cfg.setAllowedOrigins(
+                        Arrays.asList(
+                                "http://localhost:5173",
+                                "http://localhost:3000"
+                        )
+                );
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setExposedHeaders(Arrays.asList(("Authorization")));
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setMaxAge(3600L);
+                return cfg;
+            }
+        };
     }
 
     @Bean
